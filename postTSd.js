5fd89d7425c0af643f0ed286d6d2704d47fc1f62
USER <user@AD01739724.local>
2021-07-11 15:07:44 +0900

    update

--- postTSd.js
+++ postTSd.js
@@ -1,19 +1,43 @@
 import fs from 'fs';
+import path from 'path';
+const __dirname = path.resolve();
 
+const DS_STORE = '.DS_Store';
 const basePath = `@orange4glace/vs-lib`;
 
-fs.readdirSync(`types/${basePath}`)
-  .forEach(basedir => {
-    const libs = fs.readdirSync(`types/${basePath}/${basedir}`);
-    for (const lib of libs) {
-      const files = fs.readdirSync(`types/${basePath}/${basedir}/${lib}`);
-      for (const file of files) {
-        const fileWithoutExtension = file.replace('.d.ts', '');
-        fs.mkdirSync(`types/${basePath}/${basedir}/${lib}/${fileWithoutExtension}`);
-        fs.renameSync(
-          `types/${basePath}/${basedir}/${lib}/${file}`,
-          `types/${basePath}/${basedir}/${lib}/${fileWithoutExtension}/index.d.ts`
-        );
-      }
+// fs.readdirSync(`types/${basePath}`)
+//   .forEach(basedir => {
+//     const libs = fs.readdirSync(`types/${basePath}/${basedir}`);
+//     for (const lib of libs) {
+//       const files = fs.readdirSync(`types/${basePath}/${basedir}/${lib}`);
+//       for (const file of files) {
+//         const fileWithoutExtension = file.replace('.d.ts', '');
+//         fs.mkdirSync(`types/${basePath}/${basedir}/${lib}/${fileWithoutExtension}`);
+//         fs.renameSync(
+//           `types/${basePath}/${basedir}/${lib}/${file}`,
+//           `types/${basePath}/${basedir}/${lib}/${fileWithoutExtension}/index.d.ts`
+//         );
+//       }
+//     }
+//   })
+
+function search(basePath, path) {
+  const fullPath = `${basePath}/${path}`;
+  fs.readdirSync(fullPath).forEach(child => {
+    if (child === DS_STORE) return;
+    const targetPath = `${fullPath}/${child}`;
+    if (fs.lstatSync(targetPath).isDirectory()) {
+      search(basePath, `${path}/${child}`);
     }
-  })
+    else {
+      const pathWithoutExtension = targetPath.replace('.d.ts', '');
+      fs.mkdirSync(pathWithoutExtension);
+      fs.renameSync(
+        targetPath,
+        `${pathWithoutExtension}/index.d.ts`
+      );
+    }
+  });
+}
+
+search(__dirname, 'types');
--- rollup.config.js
+++ rollup.config.js
@@ -1,52 +1,108 @@
 import fs from 'fs';
 import util from 'util';
 import typescript from '@rollup/plugin-typescript';
+import styles from "rollup-plugin-styles";
 
 const libPath = '@orange4glace/vs-lib';
-const basePath = `lib/${libPath}`;
+const basePath = __dirname + `/lib/${libPath}`;
 const external = id => id.startsWith('@orange4glace/vs-lib/');
 
-const c = [];
-fs.readdirSync(basePath)
-  .forEach(basedir => {
-    const libs = fs.readdirSync(`${basePath}/${basedir}`);
-    for (const lib of libs) {
-      const files = fs.readdirSync(`${basePath}/${basedir}/${lib}`);
-      for (const file of files) {
-        const fileWithoutExtension = file.replace('.ts', '');
-        c.push({
-          input: `${basePath}/${basedir}/${lib}/${file}`,
+const DS_STORE = '.DS_Store';
+
+
+function search(path, depth = 0, result = []) {
+  fs.readdirSync(path).forEach(file => {
+    if (file === DS_STORE) return;
+    const targetPath = `${path}/${file}`;
+    if (fs.lstatSync(targetPath).isDirectory()) {
+      search(targetPath, depth + 1, result);
+    }
+    else {
+      if ((targetPath.endsWith('.ts') && !targetPath.endsWith('.d.ts')) || targetPath.endsWith('.js')) {
+        const pathWithoutExtensions = targetPath.replace('.ts', '').replace('.js', '');
+        const outputPath = pathWithoutExtensions.replace('lib/@orange4glace/vs-lib/', '');
+        const relativePath = pathWithoutExtensions.replace(`${__dirname}/lib/`, '');
+        const relativeToBasePathPrefix = '../'.repeat(depth - 1).slice(0, -1);
+        result.push({
+          input: targetPath,
           output: [
             {
-              file: `${basedir}/${lib}/${fileWithoutExtension}/index.js`,
+              file: `${outputPath}/index.js`,
               format: 'cjs',
-              paths: id => id.startsWith(libPath) && `${id.replace(libPath, '../../..')}/index.js`
+              paths: id => id.startsWith(libPath) && `${id.replace(libPath, relativeToBasePathPrefix)}/index.js`
             }
           ],
           external,
           plugins: [
             typescript(),
+            styles(),
             {
               writeBundle(bundle) {
-                fs.writeFileSync(`${basedir}/${lib}/${fileWithoutExtension}/package.json`, JSON.stringify({
+                fs.writeFileSync(`${outputPath}/package.json`, JSON.stringify({
                   main: './index',
                   types: './index.d.ts'
                 }, null, ' '));
-                fs.writeFileSync(`${basedir}/${lib}/${fileWithoutExtension}/index.d.ts`,
-                    `export * from '../../../types/${libPath}/${basedir}/${lib}/${fileWithoutExtension}/index';`);
+                fs.writeFileSync(`${outputPath}/index.d.ts`,
+                    `export * from '${relativeToBasePathPrefix}/types/${relativePath}/index';`);
               }
             }
           ]
         });
-
       }
     }
   })
+  return result;
+}
+
+// const c = [];
+// fs.readdirSync(basePath)
+//   .forEach(basedir => {
+//     if (!fs.lstatSync(`${basePath}/${basedir}`).isDirectory()) return;
+//     const libs = fs.readdirSync(`${basePath}/${basedir}`);
+//     console.log(libs);
+//     for (const lib of libs) {
+//       console.log(lib);
+//       if (!fs.lstatSync(`${basePath}/${basedir}/${lib}`).isDirectory()) continue;
+//       const files = fs.readdirSync(`${basePath}/${basedir}/${lib}`);
+//       console.log(`${basePath}/${basedir}/${lib}`);
+//       for (const file of files) {
+//         if (file === DS_STORE) continue;
+//         const fileWithoutExtension = file.replace('.ts', '');
+//         console.log(`${basePath}/${basedir}/${lib}/${file}`, `${basedir}/${lib}/${fileWithoutExtension}/index.js`, libPath);
+//         c.push({
+//           input: `${basePath}/${basedir}/${lib}/${file}`,
+//           output: [
+//             {
+//               file: __dirname + `/${basedir}/${lib}/${fileWithoutExtension}/index.js`,
+//               format: 'cjs',
+//               paths: id => id.startsWith(libPath) && `${id.replace(libPath, '../../..')}/index.js`
+//             }
+//           ],
+//           external,
+//           plugins: [
+//             typescript(),
+//             {
+//               writeBundle(bundle) {
+//                 console.log('basedir = ', basedir);
+//                 fs.writeFileSync(`${basedir}/${lib}/${fileWithoutExtension}/package.json`, JSON.stringify({
+//                   main: './index',
+//                   types: './index.d.ts'
+//                 }, null, ' '));
+//                 fs.writeFileSync(`${basedir}/${lib}/${fileWithoutExtension}/index.d.ts`,
+//                     `export * from '../../../types/${libPath}/${basedir}/${lib}/${fileWithoutExtension}/index';`);
+//               }
+//             }
+//           ]
+//         });
+
+//       }
+//     }
+//   })
 
 const config = [
-  ...c
+  ...search(`${__dirname}/lib`)
 ];
 
-console.log(util.inspect(config, false, 100));
+// console.log(util.inspect(config, false, 100));
 
 export default config;
